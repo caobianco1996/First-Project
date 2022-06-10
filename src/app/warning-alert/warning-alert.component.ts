8141dbc0ce6e0e02f08b6769d61ec6fce8a6b059
caobianco1996 <caiocaobianco1@hotmail.com>
2022-06-10 14:52:39 -0300

    Alterações

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: '<p> this is a warning, you are danger!</p>',
+  styleUrls: ['./warning-alert.component.css'],
+})
+export class WarningAlertComponent {}
