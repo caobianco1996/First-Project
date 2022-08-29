10cd164e4956e1e303b380951823cbd7919f81a6
caobianco1996 <caiocaobianco1@hotmail.com>
2022-08-29 15:04:26 -0300

    event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,11 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
+  //template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was crated!';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {}
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created';
+  }
 }
